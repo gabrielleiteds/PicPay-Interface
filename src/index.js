2a6823da146c5b90cb2f989e40c6129ada06ca3e
leitegs <leitedasilvag@gmail.com>
2020-06-16 17:12:30 -0300

    header from homepage created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native'; 
-import Navigation from './navigation'; 
+import { NavigationContainer } from '@react-navigation/native';
+import Navigation from './navigation';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Navigation />
-    </NavigationContainer>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#000"/>
+      <NavigationContainer>
+        <Navigation />
+      </NavigationContainer>
+    </>
   );
 }
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,8 +1,19 @@
-import React from 'react' ;
-import {Text} from 'react-native'; 
-
+import React from 'react';
+import { Text } from 'react-native';
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
+import { Wrapper, Header, BalanceContainer, BalanceTitle, Balance } from './styles';
 export default function Home() {
-    return(
-        <Text>Home</Text>
+    return (
+        <Wrapper>
+            <Header>
+                <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c86e" />
+                <BalanceContainer>
+                    <BalanceTitle>Meu saldo</BalanceTitle>
+                    <Balance>R$ 0,00</Balance>
+                </BalanceContainer>
+
+                <AntDesign name="gift" size={30} color="#10c86e" />
+            </Header>
+        </Wrapper>
     )
 } 
